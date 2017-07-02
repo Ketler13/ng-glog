370b483fc352f505ceff378cdf4ca702d0ac88ac
Ketler <ketler13@yandex.ru>
2017-07-03 00:02:18 +0300

    add subject to auth.server

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { Http, Headers, RequestOptions } from '@angular/http';
 
+import { Observable } from 'rxjs/Observable';
+import { Subject } from 'rxjs/Subject';
 import 'rxjs/add/operator/map';
 
 import { User } from './user';
@@ -10,6 +12,7 @@ export class AuthService {
   //private BASE_URL: string = 'http://ketler-glog.herokuapp.com/api/';
   private BASE_URL = 'http://127.0.0.1:3000/api/';
   private headers: Headers;
+  loginChecker: Subject<any>;
   isLoggedIn: boolean;
   user: User;
   token: string;
@@ -17,6 +20,7 @@ export class AuthService {
   constructor(private http: Http) {
     this.headers = new Headers({ 'Content-Type': 'application/json' });
     this.isLoggedIn = false;
+    this.loginChecker = new Subject();
   }
 
   login(data) {
@@ -28,6 +32,7 @@ export class AuthService {
         this.isLoggedIn = true;
         this.user = data.user;
         this.token = data.token;
+        this.loginChecker.next(true);
         return true;
       });
   }
@@ -36,6 +41,13 @@ export class AuthService {
     const url = this.BASE_URL + 'register';
     this.http.post(url, data, this.headers)
       .subscribe(console.log);
+  }
+
+  logout() {
+    this.isLoggedIn = false;
+    this.user = null;
+    this.token = null;
+    this.loginChecker.next(false);
   }
 
 }
